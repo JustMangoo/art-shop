b45181346ab3b6890fb0c4290c3c8854d6d3ab3d
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2023-11-08 01:30:53 +0200

    animation attempt

--- src/components/SlideComp.vue
+++ src/components/SlideComp.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="slide">
-    <transition name="slide">
-      <slot />
+    <transition name="slide" v-show="true">
+      <div v-if="true">
+        <slot />
+      </div>
     </transition>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
-<style scoped>
-  
+<style lang="scss">
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.7 ease-in-out;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+}
 </style>
