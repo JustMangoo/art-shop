fc11a9752508770eb9234596611b8266519e1a81
JustMangoo <daugatsa@gmail.com>
2023-11-08 15:54:03 +0200

    Originals setup

---
+++ src/components/OriginalsSection.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="originals">
+        <h1>Oriģināls</h1>
+        <div class="container">
+            <div class="accent">
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .originals {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        border: 2px solid black;
+
+        .container {
+            border: 2px solid red;
+            width: 100%;
+            height: 40vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            .accent {
+                background-color: var(--primary);
+                width: 90%;
+                height: 90%;
+            }
+        }
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,18 @@
   <div class="home"></div>
   <Navigation />
   <Hero />
+  <Originals />
 </template>
 
 <script>
 import Navigation from "../components/NavBar.vue";
 import Hero from "../components/HeroSection.vue";
+import Originals from "../components/OriginalsSection.vue";
 
 
 export default {
   name: "HomeView",
-  components: { Navigation, Hero },
+  components: { Navigation, Hero, Originals },
 };
 </script>
 
