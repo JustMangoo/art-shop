d882b50e881c6ab6d62feaa6da575269bf9946e9
JustMangoo <daugatsa@gmail.com>
2023-11-09 12:37:35 +0200

    Origals sections changes

--- src/components/OriginalsSection.vue
+++ src/components/OriginalsSection.vue
@@ -2,9 +2,11 @@
     <section class="originals">
         <h1>Oriģināls</h1>
         <div class="container">
+            <img src="../assets/images/hero-1.jpg" alt="" class="left">
             <div class="accent">
 
             </div>
+            <img src="../assets/images/hero-2.jpg" alt="" class="right">
         </div>
     </section>
 </template>
@@ -24,17 +26,46 @@
         border: 2px solid black;
 
         .container {
+            position: relative;
             border: 2px solid red;
-            width: 100%;
-            height: 40vh;
+            width: 80rem;
+            max-width: 90%;
+            height: 50rem;
             display: flex;
+            direction: row;
             align-items: center;
             justify-content: center;
+            
 
             .accent {
-                background-color: var(--primary);
+                position: absolute;
                 width: 90%;
-                height: 90%;
+                height: 35rem;
+                max-height: 90rem;
+                background-color: var(--primary);
+            }
+            img {
+                position: absolute;
+                z-index: 80;
+                object-fit: cover;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                
+            }
+
+            .left {
+                height: 21vh;
+                left: 0;
+                top:0;
+                aspect-ratio: 4/5;
+            }
+
+            .right {
+                height: 40vh;
+                right: 10%;
+                bottom: 0;
+                aspect-ratio: 3/4;
             }
         }
     }
